334696b1c1482bdc320705170e15583bc5a55b02
absanyal <absanyal.ABS@gmail.com>
2024-06-18 17:45:29 -0400

    Update bounds and bin ranges for bounded Brownian motion simulation

--- bounded-brownian-motion/main.py
+++ bounded-brownian-motion/main.py
@@ -5,18 +5,18 @@ from modules.walker import walker
 from modules.wall import wall
 
 # Define the number of trials and iterations
-num_trials = 100000
+num_trials = 50000
 num_iters = 1000
 
 # Define the wall
-xmin = -20
-xmax = 20
+xmin = -30
+xmax = 30
 
-ymin = -20
-ymax = 20
+ymin = -30
+ymax = 30
 
-zmin = -20
-zmax = 20
+zmin = -30
+zmax = 30
 
 this_wall = wall(xmin, xmax, ymin, ymax, zmin, zmax)
 
@@ -27,7 +27,7 @@ Dz = 1.0
 D = np.array([Dx, Dy, Dz])
 
 x0 = 0
-y0 = 15
+y0 = 0
 z0 = 0
 
 dt = 0.02
@@ -39,74 +39,71 @@ final_x = np.zeros(num_trials)
 final_y = np.zeros(num_trials)
 final_z = np.zeros(num_trials)
 
-bin_x = np.linspace(xmin, xmax, 100)
-bin_y = np.linspace(ymin, ymax, 100)
-bin_z = np.linspace(zmin, zmax, 100)
+bin_x = np.arange(xmin, xmax + 1, 1.0)
+bin_y = np.arange(xmin, xmax + 1, 1.0)
+bin_z = np.arange(xmin, xmax + 1, 1.0)
 
 for trial in range(num_trials):
-    
+
     this_walker.reset()
     for ti in range(num_iters):
         this_walker.step()
-    
+
     final_x[trial] = this_walker.x
     final_y[trial] = this_walker.y
     final_z[trial] = this_walker.z
-    
-    if trial % 100 == 0:
-        
+
+    if trial % 100 == 0 and trial > 0:
+
         print('Trial:', trial)
-        
+
         fig, ax = plt.subplots(1, 3, figsize=(15, 5), tight_layout=True)
-        
-        ax[0].hist(final_x[:trial], bins=bin_x, density=True)
+
+        ax[0].hist(final_x[:trial], bins=bin_x,
+                   density=True, color='b', rwidth=0.8)
         ax[0].set_xlabel('x')
         ax[0].set_ylabel('Density')
         ax[0].set_xlim(xmin, xmax)
-        
-        ax[1].hist(final_y[:trial], bins=bin_y, density=True)
+
+        ax[1].hist(final_y[:trial], bins=bin_y,
+                   density=True, color='b', rwidth=0.8)
         ax[1].set_xlabel('y')
         ax[1].set_ylabel('Density')
         ax[1].set_xlim(ymin, ymax)
-        
-        ax[2].hist(final_z[:trial], bins=bin_z, density=True)
+
+        ax[2].hist(final_z[:trial], bins=bin_z,
+                   density=True, color='b', rwidth=0.8)
         ax[2].set_xlabel('z')
         ax[2].set_ylabel('Density')
         ax[2].set_xlim(zmin, zmax)
-        
+
         plt.savefig('histograms.png')
-        
+
         plt.close()
-        
+
         fig, ax = plt.subplots(1, 3, figsize=(15, 5), tight_layout=True)
-        
-        ax[0].scatter(final_x[:trial], final_y[:trial], s=1)
+
+        ax[0].scatter(final_x[:trial], final_y[:trial], s=5, c='b', alpha=0.2)
         ax[0].set_xlabel('x')
         ax[0].set_ylabel('y')
         ax[0].set_xlim(xmin, xmax)
         ax[0].set_ylim(ymin, ymax)
         ax[0].set_title('x - y projection')
-        
-        ax[1].scatter(final_x[:trial], final_z[:trial], s=1)
+
+        ax[1].scatter(final_x[:trial], final_z[:trial], s=5, c='b', alpha=0.2)
         ax[1].set_xlabel('x')
         ax[1].set_ylabel('z')
         ax[1].set_xlim(xmin, xmax)
         ax[1].set_ylim(zmin, zmax)
         ax[1].set_title('x - z projection')
-        
-        ax[2].scatter(final_y[:trial], final_z[:trial], s=1)
+
+        ax[2].scatter(final_y[:trial], final_z[:trial], s=5, c='b', alpha=0.2)
         ax[2].set_xlabel('y')
         ax[2].set_ylabel('z')
         ax[2].set_xlim(ymin, ymax)
         ax[2].set_ylim(zmin, zmax)
         ax[2].set_title('y - z projection')
-        
+
         plt.savefig('finalpos.png')
-        
+
         plt.close()
-    
-        
-        
-    
-    
-    
