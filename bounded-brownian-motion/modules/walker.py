553ff6d90056cc7d0a4ee13e5bd04af3bd5f011f
absanyal <absanyal.ABS@gmail.com>
2024-06-20 13:14:55 -0400

    refactor: Improve collision detection and added adsorption handling in walker.py

--- bounded-brownian-motion/modules/walker.py
+++ bounded-brownian-motion/modules/walker.py
@@ -13,6 +13,8 @@ class walker:
         self.__y0 = y0
         self.__z0 = z0
         
+        self.__isadsorped = False
+        
         if wall is not None:
             self.__xmin = wall.xmin
             self.__xmax = wall.xmax
@@ -27,6 +29,7 @@ class walker:
         self.__x = self.__x0
         self.__y = self.__y0
         self.__z = self.__z0
+        self.__isadsorped = False
         
     @property
     def D(self):
@@ -60,7 +63,12 @@ class walker:
     def dt(self):
         return self.__dt
     
-    # def check_collision(self):
+    def __roll_for_adsorption(self):
+        r = np.random.uniform()
+        if r < self.__wall.adsorption_p:
+            return True
+        else:
+            return False
         
     
     def step(self):
@@ -68,40 +76,61 @@ class walker:
         dy = np.sqrt(2 * self.Dy * self.dt) * np.random.normal()
         dz = np.sqrt(2 * self.Dz * self.dt) * np.random.normal()
         
-        potential_x = self.__x + dx
-        potential_y = self.__y + dy
-        potential_z = self.__z + dz
+        proposed_x = self.__x + dx
+        proposed_y = self.__y + dy
+        proposed_z = self.__z + dz
         
         x_inside = 0
         y_inside = 0
         z_inside = 0
         
         full_inside = x_inside * y_inside * z_inside
+        adsorped = 0
         
         if self.__wall is not None:
-            while not full_inside:
-                if potential_x < self.__xmin or potential_x > self.__xmax:
-                    dx = np.sqrt(2 * self.Dx * self.dt) * np.random.normal()
-                    potential_x = self.__x + dx
+            while not full_inside and not adsorped:
+                if proposed_x < self.__xmin or proposed_x > self.__xmax:
+                    adsorped = self.__roll_for_adsorption()
+                    if adsorped:
+                        self.__isadsorped = True
+                        break
+                    else:
+                        dx = np.sqrt(2 * self.Dx * self.dt) * np.random.normal()
+                        proposed_x = self.__x + dx
                 else:
                     x_inside = 1
                 
-                if potential_y < self.__ymin or potential_y > self.__ymax:
-                    dy = np.sqrt(2 * self.Dy * self.dt) * np.random.normal()
-                    potential_y = self.__y + dy
+                if proposed_y < self.__ymin or proposed_y > self.__ymax:
+                    adsorped = self.__roll_for_adsorption()
+                    if adsorped:
+                        self.__isadsorped = True
+                        break
+                    else:
+                        dy = np.sqrt(2 * self.Dy * self.dt) * np.random.normal()
+                        proposed_y = self.__y + dy
                 else:
                     y_inside = 1
                 
-                if potential_z < self.__zmin or potential_z > self.__zmax:
-                    dz = np.sqrt(2 * self.Dz * self.dt) * np.random.normal()
-                    potential_z = self.__z + dz
+                if proposed_z < self.__zmin or proposed_z > self.__zmax:
+                    adsorped = self.__roll_for_adsorption()
+                    if adsorped:
+                        self.__isadsorped = True
+                        break
+                    else:
+                        dz = np.sqrt(2 * self.Dz * self.dt) * np.random.normal()
+                        proposed_z = self.__z + dz
                 else:
                     z_inside = 1
                 
                 full_inside = x_inside * y_inside * z_inside
         
-        self.__x = potential_x
-        self.__y = potential_y
-        self.__z = potential_z
+        self.__x = proposed_x
+        self.__y = proposed_y
+        self.__z = proposed_z
+        
+
+    @property
+    def isadsorped(self):
+        return self.__isadsorped
             
         
