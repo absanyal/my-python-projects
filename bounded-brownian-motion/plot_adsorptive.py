83f2f5fdc5c2328a98d4f9ec203af9785b877f04
absanyal <absanyal.ABS@gmail.com>
2024-06-21 16:16:28 -0400

    refactor: Update density calculation and filtering in plot_adsorptive.py

--- bounded-brownian-motion/plot_adsorptive.py
+++ bounded-brownian-motion/plot_adsorptive.py
@@ -3,86 +3,90 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 from modules.dosmaker import make_dos as mdos
+from modules.diffusion_solutions import reflective_soln, adsorptive_soln
 
 mpl.rcParams['axes.labelsize'] = 20
 mpl.rcParams['xtick.labelsize'] = 20
 mpl.rcParams['ytick.labelsize'] = 20
 mpl.rcParams['legend.fontsize'] = 16
 
+plot_histogram = 0
+
 trials, final_x_a = np.loadtxt('adsorptive_walker.txt', unpack=True)
 
-final_x_i1 = np.loadtxt('adsorptive_i1.txt', unpack=True, usecols=1)
-final_x_i2 = np.loadtxt('adsorptive_i2.txt', unpack=True, usecols=1)
+Dx, Dy, Dz, x0, y0, z0, dt, distance_from_wall, num_iters, p_adsorption = np.loadtxt(
+    'adsorptive_info.txt', unpack=True)
 
-min_x = min(min(final_x_a), min(final_x_i1), min(final_x_i2))
-max_x = max(max(final_x_a), max(final_x_i1), max(final_x_i2))
+t_final = num_iters * dt
+
+min_x = min(final_x_a)
+max_x = max(final_x_a)
 
 xmax = max(final_x_a)
 
 bin_x = np.arange(min_x, max_x + 1, 0.5)
 
-padding = 0
-gamma = 0.25
+padding = 1
+gamma = 0.3
 
 w_list = np.linspace(min_x - padding, max_x + padding, 1000)
 x_wall = max(final_x_a)
 
+x_ticks1 = np.arange(0, max_x + 1, 10)
+x_ticks2 = np.arange(0, min_x + 1, -10)
 
-density_r_unfiltered = mdos(w_list, final_x_a, gamma)
+x_ticks = np.concatenate((x_ticks2, x_ticks1))
 
-density_r = np.zeros_like(density_r_unfiltered)
+density_a_unfiltered = mdos(w_list, final_x_a, gamma)
+
+density_a = np.zeros_like(density_a_unfiltered)
 
 for wi, w in enumerate(w_list):
     if w < x_wall:
-        density_r[wi] = density_r_unfiltered[wi]
+        density_a[wi] = density_a_unfiltered[wi]
     else:
-        density_r[wi] = 0
+        density_a[wi] = 0
 
-density_r = np.array(density_r)
+density_a = np.array(density_a)
 
-area_r = np.trapz(density_r, w_list)
-density_r /= area_r
+area_a = np.trapz(density_a, w_list)
+density_a /= area_a
 
-density_i1 = mdos(w_list, final_x_i1, gamma)
-density_i2 = mdos(w_list, final_x_i2, gamma)
+# Plot walker density
 
-total_image_unfiltered = (density_i1 - density_i2)
+fig = plt.figure(figsize=(5, 5), tight_layout=True)
 
-total_image = np.zeros_like(total_image_unfiltered)
-for w in w_list:
-    for wi, w in enumerate(w_list):
-        if w < x_wall:
-            total_image[wi] = total_image_unfiltered[wi]
-        else:
-            total_image[wi] = 0
+plt.plot(w_list, density_a, label='Simulation',
+         color='b', linewidth=1, alpha=1.0)
 
-total_image = np.array(total_image)
+if plot_histogram:
+    plt.hist(final_x_a, bins=bin_x, density=True, color='b', alpha=0.5)
 
-total_image_area = np.trapz(total_image, w_list)
-total_image /= total_image_area
+solution_un = adsorptive_soln(w_list - x_wall, distance_from_wall, Dx, t_final)
+solution = np.zeros_like(solution_un)
+for wi, w in enumerate(w_list):
+    if w < x_wall:
+        solution[wi] = solution_un[wi]
+    else:
+        solution[wi] = 0
 
-plt.figure(figsize=(9, 6), constrained_layout=True)
+solution_nf = np.trapz(solution, w_list)
+solution /= solution_nf
 
-plt.plot(w_list, density_r, label='Adsorptive',
-         color='b', linewidth=1, alpha=0.7)
-
-plt.plot(w_list, density_i1, label=r'$I_1$',
-         color='k', linewidth=1, alpha=0.5, linestyle='dashed')
-plt.plot(w_list, density_i2, label=r'$I_2$',
-         color='k', linewidth=1, alpha=0.5, linestyle='dashed')
-plt.plot(w_list, total_image, label=r'$I_1 - I_2$',
-         color='r', linewidth=1, alpha=0.5)
+plt.plot(w_list, solution, label='Theory',
+         color='r', linewidth=1, alpha=1.0)
 
 plt.xlabel(r'$x$')
 plt.ylabel(r'$P(x)$')
+
 plt.xlim(min_x - padding, max_x + padding)
 plt.ylim(bottom=0)
 
+plt.xticks(x_ticks)
+
 plt.axvline(x=xmax, color='k', linestyle='dashed',
-            label='Wall', linewidth=2.0)
+            label='Wall', linewidth=1.0, alpha=0.5)
 
-plt.legend(bbox_to_anchor=(1.0, 1), loc='upper left')
-
+# ax.legend(bbox_to_anchor=(1.0, 1), loc='upper left')
+plt.legend(loc='upper left', fancybox=True, framealpha=0.3)
 plt.savefig('adsorptive_dos.png')
-
-plt.close()
